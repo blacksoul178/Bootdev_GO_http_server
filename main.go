dbf838be5b737b0ea7c8557e32b99da2beb0284c
Marc-André <blacksoul178@gmail.com>
2025-08-27 13:36:48 -0400

    ping

--- main.go
+++ main.go
@@ -11,9 +11,13 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 	serveMux.Handle("assets/logo.png", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	http.ListenAndServe(server.Addr, server.Handler)
 
 }
